c6bb9ff6a7075dbcfbb0c9c732e8a7bb2915ea68
Greg Miller <gmill002@gmail.com>
2014-05-06 11:54:16 -0700

    updates and remove path lib

---
+++ test_omr/run_tests_linux.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd ..
+
+# build documents
+#python epydoc.py --config DocConfig.py
+
+# check module can import by trying help interface 
+python omr/omrcmd.py --help 2>&1 | tee test_omr/test_results.txt
+
+# run the unit tests
+rm -rf test_omr/test_tmp
+nosetests -v test_omr/test_omr.py 2>&1 | tee -a test_omr/test_results.txt
+rm -rf test_omr/test_tmp
+
+# run gui
+#rm -rf test_omr/test_data/OMR
+#python omr/omrcmd.py
+#rm -rf test_omr/test_data/OMR
